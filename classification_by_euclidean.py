171d2282a045d433918036eb25d237af52b19753
Thepnathi <thepnathi@hotmail.com>
2020-11-23 15:15:20 +0000

    Split the classfication.py to two types of histogram classication .py files

---
+++ classification_by_euclidean.py
@@ -0,0 +1,76 @@
+from typing import List, Dict
+import collections, math
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import generate_codebook as gc
+import helper as hp
+
+################################################################################
+# Result visualisations
+################################################################################
+
+def display_image_with_label(label, image_path):
+    img = mpimg.imread(image_path)
+    imgplot = plt.imshow(img)
+    plt.title(label)
+    plt.show()
+
+################################################################################
+# Step 4. Classification
+################################################################################
+
+def euclidean_distance(test_hist, train_hist) -> int:
+    total = 0
+    for codeword_freq in train_hist:
+        total += pow(test_hist[codeword_freq] - train_hist[codeword_freq], 2)
+    return math.sqrt(total)
+
+def apply_nearest_neighbour(test_hist, training_hist_by_classes, limit=None) -> Dict[str, int]:
+    # Computes the nearest neighbour of the different class to the test histogram
+    # limit is used to limit the amount of training histogram to use for each class
+    result = collections.defaultdict(int)
+
+    for class_type in training_hist_by_classes:
+        n = limit if limit else len(training_hist_by_classes[class_type])
+        for hist in range(n):
+            train_hist = training_hist_by_classes[class_type][hist]
+            result[class_type] += euclidean_distance(test_hist, train_hist)
+
+    return result
+
+def label_classification(test_hist: dict, train_hist: dict, limit=None):
+    # Classify one test histogram given multiple training histograms of multiple classes
+    result = apply_nearest_neighbour(test_hist, train_hist, limit)
+    label = None
+
+    for key in result:
+        if not label:
+            label = (key[0], result[key])
+        elif result[key] < label[1]:
+            label = (key[0], result[key])
+        
+    return label
+
+def label_all_test_images(limit=None):
+    # Get the paths of all histogram files
+    training_path, test_path = hp.get_histogram_paths()
+
+    # Load all histogram binary file name
+    all_test_hist = hp.load_all_histograms(test_path)
+    all_training_hist = hp.load_all_histograms(training_path)
+
+    for class_type in all_test_hist:
+        correct_label, amount = 0, 0
+        for hist in all_test_hist[class_type]:
+            result = label_classification(hist, all_training_hist, limit)
+            amount += 1
+            if result[0] == class_type[0]:
+                correct_label += 1
+        percentage_correct = int((correct_label / amount) * 100)
+        print(f'{class_type[0]} correct label is {percentage_correct}%')
+
+if __name__ == "__main__":
+    # Classification
+    s = hp.get_image_paths("jpg")
+    img = s['airplanes'][0]
+    display_image_with_label("airplanes", img)
