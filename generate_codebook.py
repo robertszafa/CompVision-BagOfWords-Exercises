Fix bug and add SAD distance calculation.

generate_codebook.py
@@ -1,28 +1,25 @@
-import random, time, re, os
+import random, time
 import cv2
 import numpy as np
-
-from helper import load_descriptors, save_to_pickle, euclidean_distance, mean
-from helper import DATASET_DIR, CLASSES, CODEBOOK_FILE_TRAIN, SMALL_CODEBOOK_FILE_TRAIN, UNLIMITED_CODEBOOK_FILE_TRAIN, UNLIMITED_SMALL_CODEBOOK_FILE_TRAIN
-
-# Speed up k-NN
 import multiprocessing as mp
 
+import helper as hp
 
 ################################################################################
 # Step 2. Dictionary generation
 ################################################################################
-def find_closest_neighbour_idx(neighbours_candidate_pair):
+def find_closest_neighbour_idx(neighbours_candidate_dist_func):
     """
-    Given a tuple of (neighbours, candidate), return the index of the 1-kNN of candidate in neighbours.
+    Given a tuple of (neighbours, candidate, dist_func),
+    return the index of the 1-kNN of candidate in neighbours.
     """
-    neighbours, candidate = neighbours_candidate_pair
+    neighbours, candidate, dist_func = neighbours_candidate_dist_func
 
     closest_idx = 0
-    curr_dist = euclidean_distance(candidate, neighbours[closest_idx])
+    curr_dist = dist_func(candidate, neighbours[closest_idx])
 
     for i in range(len(neighbours)):
-        this_dist = euclidean_distance(candidate, neighbours[i])
+        this_dist = dist_func(candidate, neighbours[i])
 
         if this_dist < curr_dist:
             closest_idx = i
@@ -30,7 +27,7 @@ def find_closest_neighbour_idx(neighbours_candidate_pair):
 
     return closest_idx
 
-def gen_codebook(feature_descriptors, fname, num_words=500):
+def gen_codebook(feature_descriptors, fname, dist_func=hp.euclidean_distance, num_words=500):
     start_time = time.time()
 
     # Initialise. Randomly choose num_words feature descriptors as cluster centres.
@@ -39,43 +36,37 @@ def gen_codebook(feature_descriptors, fname, num_words=500):
     for i in random_idxs:
         codebook.append(feature_descriptors[i])
 
-    # Do, while there were any changes in any cluster.
-    do_next_iter = True
+    # Do clustering while there are any changes in any cluster centre, but not more than max_iter.
     max_iter = 10
-    iteration = 0
-    while do_next_iter and iteration < max_iter:
-        iteration += 1
-        do_next_iter = False
-
+    for iteration in range(1, max_iter+1):
         # Find the indexes of the nearest cluster for each descriptor.
         # This step is easily parallelizable.
         with mp.Pool(mp.cpu_count()) as pool:
-            # pool.map takes one argument, so we have to zip codebook and descriptor together.
-            # Note that we need unique copies of the codebook since each process might execute
-            # on different CPUs.
-            neighbours_candidate_pair = [(codebook, descriptor) for descriptor in descriptors]
-            closest_cluster_idxs = pool.map(find_closest_neighbour_idx, neighbours_candidate_pair)
+            # pool.map takes one argument.
+            map_input = [(codebook, descriptor, dist_func) for descriptor in feature_descriptors]
+            closest_cluster_idxs = pool.map(find_closest_neighbour_idx, map_input)
 
         # Collect all the descrtiptors mapped to the same keyword from the calculated indexes.
-        # pool.map results are ordered,
-        # i.e. the output closest_cluster_idxs[i] corresponds to the descriptor[i]
         cluster_vectors_map = [[word] for word in codebook]
-        for i in closest_cluster_idxs:
-            cluster_vectors_map[closest_cluster_idxs[i]].append(descriptors[i])
+        for i in range(len(closest_cluster_idxs)):
+            # pool.map results are ordered,
+            # i.e. the output closest_cluster_idxs[i] corresponds to the descriptor[i]
+            cluster_vectors_map[closest_cluster_idxs[i]].append(feature_descriptors[i])
 
         # Calculate new cluster centers. This is also easily parallelizable.
         with mp.Pool(mp.cpu_count()) as pool:
-            new_centers = pool.map(mean, cluster_vectors_map)
+            new_centers = pool.map(hp.mean, cluster_vectors_map)
 
         # Stop if there are no more improvements to be made.
-        do_next_iter = not np.all(np.array(codebook) == np.array(new_centers))
+        if np.all(np.array(codebook) == np.array(new_centers)):
+            break
 
         # Assign new centers.
         for i in range(len(codebook)):
             codebook[i] = new_centers[i]
-
+        hp.save_to_pickle(fname, codebook)
         print(f'Finished iteration {iteration} at minute {(time.time() - start_time)/60}.')
-        save_to_pickle(fname, codebook)
+
 
     return codebook
 
@@ -87,7 +78,7 @@ if __name__ == "__main__":
 
     # Merge the descriptors from one class into a single list.
     # training_descriptors will hold ['class_name': descriptors_list] pairs
-    training_descriptors = load_descriptors(test_or_train='Training', merge_in_class=True)
+    training_descriptors = hp.load_descriptors(test_or_train='Training', merge_in_class=True)
 
     # A single list for all feature descriptors from all classes.
     # Pick the same number of descriptors from each class to prevent bias in the code book.
@@ -98,10 +89,16 @@ if __name__ == "__main__":
         capped_descriptors += random.sample(descriptors, min_len_descriptors)
         all_descriptors += descriptors
 
-    gen_codebook(capped_descriptors, fname=CODEBOOK_FILE_TRAIN, num_words=500)
-    gen_codebook(capped_descriptors, fname=SMALL_CODEBOOK_FILE_TRAIN, num_words=20)
+    gen_codebook(capped_descriptors, fname=hp.SAD_CODEBOOK_FILE_TRAIN, dist_func=hp.sad, num_words=500)
+    gen_codebook(capped_descriptors, fname=hp.SAD_SMALL_CODEBOOK_FILE_TRAIN, dist_func=hp.sad, num_words=20)
 
-    gen_codebook(all_descriptors, fname=UNLIMITED_CODEBOOK_FILE_TRAIN, num_words=500)
-    gen_codebook(all_descriptors, fname=UNLIMITED_SMALL_CODEBOOK_FILE_TRAIN, num_words=20)
+    gen_codebook(capped_descriptors, fname=hp.CODEBOOK_FILE_TRAIN, dist_func=hp.euclidean_distance, num_words=500)
+    gen_codebook(capped_descriptors, fname=hp.SMALL_CODEBOOK_FILE_TRAIN, dist_func=hp.euclidean_distance, num_words=20)
+
+    gen_codebook(all_descriptors, fname=hp.SAD_UNLIMITED_CODEBOOK_FILE_TRAIN, dist_func=hp.sad, num_words=500)
+    gen_codebook(all_descriptors, fname=hp.SAD_UNLIMITED_SMALL_CODEBOOK_FILE_TRAIN, dist_func=hp.sad, num_words=20)
+
+    gen_codebook(all_descriptors, fname=hp.UNLIMITED_CODEBOOK_FILE_TRAIN, dist_func=hp.euclidean_distance, num_words=500)
+    gen_codebook(all_descriptors, fname=hp.UNLIMITED_SMALL_CODEBOOK_FILE_TRAIN, dist_func=hp.euclidean_distance, num_words=20)
 
     print(f'Finished program in {(time.time() - start_time)/60} minutes.')