db462b7a7fff4b41bac34f20037a2679cb56b7f2
Robert Szafarczyk <robertszafa@gmail.com>
2020-11-25 19:05:29 +0000

    Update histogram generation to use different codebooks.

--- histogram_of_codewords.py
+++ histogram_of_codewords.py
@@ -4,9 +4,7 @@ import time
 import sys
 import re
 
-from helper import load_descriptors, load_keypoints, load_pickled_list, save_to_pickle, euclidean_distance
-from helper import DATASET_DIR, CLASSES, CODEBOOK_FILE_TRAIN, SMALL_CODEBOOK_FILE_TRAIN, MAP_KPS_TO_CODEWORD_FILE, MAP_KPS_TO_SMALL_CODEWORD_FILE
-
+import helper as hp
 import multiprocessing as mp
 
 ###########################################################################
@@ -15,11 +13,11 @@ import multiprocessing as mp
 ## Step 3.1
 def find_nearest_cluster_idx(descriptor, clusters):
     min_idx = 0
-    min_dist = euclidean_distance(descriptor, clusters[min_idx])
+    min_dist = hp.euclidean_distance(descriptor, clusters[min_idx])
 
     for i in range(1, len(clusters)):
-        if euclidean_distance(descriptor, clusters[i]) < min_dist:
-            min_dist = euclidean_distance(descriptor, clusters[i])
+        if hp.euclidean_distance(descriptor, clusters[i]) < min_dist:
+            min_dist = hp.euclidean_distance(descriptor, clusters[i])
             min_idx = i
 
     return min_idx
@@ -59,7 +57,7 @@ def visualize_similar_patches(map_kps_to_codewords):
     for word_idx, img_fname_keypoints_pairs in enumerate(map_kps_to_codewords):
         for img_fname, kps in img_fname_keypoints_pairs.items():
             for kp in kps:
-                title = title=f'{img_fname.split(DATASET_DIR)[1]} --> codeword_{word_idx}'
+                title = title=f'{img_fname.split(hp.DATASET_DIR)[1]} --> codeword_{word_idx}'
                 draw_keypoint(img_fname, int(kp[0][0]), int(kp[0][1]), int(kp[1]), title)
 
 ## Step 3.4
@@ -76,35 +74,27 @@ def normalise_histogram(histogram):
     return histogram
 
 
-if __name__ == "__main__":
-    start_time = time.time()
-
-    codebook = load_pickled_list(CODEBOOK_FILE_TRAIN)
-    # codebook = load_pickled_list(SMALL_CODEBOOK_FILE_TRAIN)
-
-    training_descriptors = load_descriptors(test_or_train='Training', merge_in_class=False)
-    test_descriptors = load_descriptors(test_or_train='Test', merge_in_class=False)
+def gen_histograms(training_descriptors, test_descriptors, training_keypoints, test_keypoints,
+                   codebook, hist_file_extension='_histogram.npy', kp_diameter_threshold = 30):
+    """
+    Generate a histogram for all images from the given codebook.
+    """
 
     # Keep track of indexes of keypoints which mapped to the same codeword. One dictionary of
     # {img_fname: [keypoints]} pairs per codeword.
     map_kps_to_codewords = [dict() for _ in range(len(codebook))]
-    # We're not interested in small keypoints for the visualisation.
-    KEYPOINT_DIAMETER_THRESHOLD = 30
 
-    # The same layour for the keypoints.
-    # Note that keypoint i of a given image corresponds to descriptor i of that image.
-    training_keypoints = load_keypoints(test_or_train='Training', merge_in_class=False)
-    test_keypoints = load_keypoints(test_or_train='Test', merge_in_class=False)
-
-    for train_or_test in ['Training', 'Test']:
+    for train_or_test in ['Test', 'Training']:
         descriptors_dict = training_descriptors if train_or_test == 'Training' else test_descriptors
         keypoints_dict = training_keypoints if train_or_test == 'Training' else test_keypoints
 
         for img_class, descriptors_files in descriptors_dict.items():
             with mp.Pool(mp.cpu_count()) as pool:
                 # Pack input for map.
-                img_descriptors_codebook_pair = [(descriptors, codebook) for descriptors in descriptors_files.values()]
-                img_histograms_descriptor_to_codeword_map_pairs = pool.map(gen_histogram_of_codewords, img_descriptors_codebook_pair)
+                img_descriptors_codebook_pair = \
+                    [(descriptors, codebook) for descriptors in descriptors_files.values()]
+                img_histograms_descriptor_to_codeword_map_pairs = \
+                    pool.map(gen_histogram_of_codewords, img_descriptors_codebook_pair)
 
                 # Unpack output from map.
                 img_histograms, descriptor_to_codeword_maps = [], []
@@ -116,43 +106,91 @@ if __name__ == "__main__":
 
             # Save each image histogram to a seperate file
             for i, img_id in enumerate(descriptors_files.keys()):
-                hist_fname = f'{DATASET_DIR}/{train_or_test}/{img_class}/{img_id}_histogram.npy'
-                # hist_fname = f'{DATASET_DIR}/{train_or_test}/{img_class}/{img_id}_histogram_small.npy'
+                hist_fname = f'{hp.DATASET_DIR}/{train_or_test}/{img_class}/{img_id}{hist_file_extension}'
+                hp.save_to_pickle(hist_fname, nor_img_histograms[i])
 
-                # From Python 3.6 onwards, the standard dict type maintains insertion order by default,
-                # i.e. we can use i here to get the nor_histogram corresponding to the img_id
-                save_to_pickle(hist_fname, nor_img_histograms[i])
-
-            for descriptor_to_codeword_map in descriptor_to_codeword_maps:
+            for i, img_id in enumerate(descriptors_files.keys()):
                 # Use full img path, instead of id, for easier visualisation.
-                img_fname = f'{DATASET_DIR}/{train_or_test}/{img_class}/{img_id}.jpg'
-                for word_idx, keypoint_idxs_list in enumerate(descriptor_to_codeword_map):
+                img_fname = f'{hp.DATASET_DIR}/{train_or_test}/{img_class}/{img_id}.jpg'
+                for word_idx, keypoint_idxs_list in enumerate(descriptor_to_codeword_maps[i]):
                     # Get rid of small keypoints.
                     filtered_keypoints = []
                     for kp_idx in keypoint_idxs_list:
                         # We have saved the keypoint as [(kp_x, (kp_y), kp_diameter]
                         # Use the fact that there is a 1:1 mapping between descriptor and kypoint idxs.
                         kp = keypoints_dict[img_class][img_id][kp_idx]
-                        if kp[1] > KEYPOINT_DIAMETER_THRESHOLD:
+                        if kp[1] > kp_diameter_threshold:
                             filtered_keypoints.append(kp)
 
                 map_kps_to_codewords[word_idx][img_fname] = filtered_keypoints
 
             print(f'Finished {train_or_test}/{img_class} in {(time.time() - start_time)/60} minutes.')
 
-    # map_kps_to_codewords = load_pickled_list(MAP_KPS_TO_CODEWORD_FILE)
-    save_to_pickle(MAP_KPS_TO_CODEWORD_FILE, map_kps_to_codewords)
+    return map_kps_to_codewords
 
-    # map_kps_to_codewords = load_pickled_list(MAP_KPS_TO_SMALL_CODEWORD_FILE)
-    # save_to_pickle(MAP_KPS_TO_SMALL_CODEWORD_FILE, map_kps_to_codewords)
 
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    # codebook = hp.load_pickled_list(hp.CODEBOOK_FILE_TRAIN)
+    # codebook_small = hp.load_pickled_list(hp.SMALL_CODEBOOK_FILE_TRAIN)
+    sad_codebook = hp.load_pickled_list(hp.SAD_CODEBOOK_FILE_TRAIN)
+    # sad_codebook_small = hp.load_pickled_list(hp.SAD_SMALL_CODEBOOK_FILE_TRAIN)
+
+    # codebook_unlimited = hp.load_pickled_list(hp.UNLIMITED_CODEBOOK_FILE_TRAIN)
+    # codebook_small_unlimited = hp.load_pickled_list(hp.UNLIMITED_SMALL_CODEBOOK_FILE_TRAIN)
+    # sad_codebook_unlimited = hp.load_pickled_list(hp.SAD_UNLIMITED_CODEBOOK_FILE_TRAIN)
+    # sad_codebook_small_unlimited = hp.load_pickled_list(hp.SAD_UNLIMITED_SMALL_CODEBOOK_FILE_TRAIN)
+
+    training_descriptors = hp.load_descriptors(test_or_train='Training', merge_in_class=False)
+    test_descriptors = hp.load_descriptors(test_or_train='Test', merge_in_class=False)
+    # Note that keypoint i of a given image corresponds to descriptor i of that image.
+    training_keypoints = hp.load_keypoints(test_or_train='Training', merge_in_class=False)
+    test_keypoints = hp.load_keypoints(test_or_train='Test', merge_in_class=False)
+
+    # map_kps_to_codebook = gen_histograms(training_descriptors, test_descriptors,
+    #                                      training_keypoints, test_keypoints,
+    #                                      codebook, hist_file_extension='_histogram.npy')
+    # hp.save_to_pickle(hp.MAP_KPS_TO_CODEBOOK_FILE, map_kps_to_codebook)
+
+    # map_kps_to_codebook_small = gen_histograms(training_descriptors, test_descriptors,
+    #                                            training_keypoints, test_keypoints,
+    #                                            codebook_small, hist_file_extension='_histogram_small.npy')
+    # hp.save_to_pickle(hp.MAP_KPS_TO_SMALL_CODEBOOK_FILE, map_kps_to_codebook)
+
+    map_kps_to_sad_codebook = gen_histograms(training_descriptors, test_descriptors,
+                                               training_keypoints, test_keypoints,
+                                               sad_codebook, hist_file_extension='_histogram_sad.npy')
+    # map_kps_to_sad_codebook_small = gen_histograms(training_descriptors, test_descriptors,
+    #                                            training_keypoints, test_keypoints,
+    #                                            sad_codebook_small, hist_file_extension='_histogram_sad_small.npy')
+
+    # map_kps_to_codebook_unlimited = gen_histograms(training_descriptors, test_descriptors,
+    #                                            training_keypoints, test_keypoints,
+    #                                            codebook_unlimited, hist_file_extension='_histogram_unlimited.npy')
+    # map_kps_to_codebook_small_unlimited = gen_histograms(training_descriptors, test_descriptors,
+    #                                            training_keypoints, test_keypoints,
+    #                                            codebook_small_unlimited, hist_file_extension='_histogram_unlimited_small.npy')
+
+    # map_kps_to_sad_codebook_unlimited = gen_histograms(training_descriptors, test_descriptors,
+    #                                            training_keypoints, test_keypoints,
+    #                                            sad_codebook_unlimited, hist_file_extension='_histogram_sad_unlimited.npy')
+    # map_kps_to_sad_codebook_small_unlmited = gen_histograms(training_descriptors, test_descriptors,
+    #                                            training_keypoints, test_keypoints,
+    #                                            sad_codebook_small_unlimited, hist_file_extension='_histogram_sad_unlimited_small.npy')
+
+    #
+    # Step 3.3 Visualize some image patches that are assigned to the same codeword.
+    #
+    map_kps_to_codebook = hp.load_pickled_list(hp.MAP_KPS_TO_CODEBOOK_FILE)
     # Put most matched codewords and the corresponding keypoints to the front.
     # Note that we don't care towhich specific codeword given keypoints match, we just care about
     # the fact that they match to the same one.
     num_of_kps = lambda d : np.sum([len(v) for v in d.values()])
-    map_kps_to_codewords.sort(key=num_of_kps, reverse=True)
+    map_kps_to_codebook.sort(key=num_of_kps, reverse=True)
 
-    # Step 3.3 Visualize some image patches that are assigned to the same codeword.
-    visualize_similar_patches(map_kps_to_codewords)
+    visualize_similar_patches(map_kps_to_codebook)
+
 
     print(f'Finished program in {(time.time() - start_time)/60} minutes.')
